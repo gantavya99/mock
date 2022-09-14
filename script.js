f1fe2d3900b18699a394255fef9dc0ba7a2bc626
Gantavya Saraswat <gantavya99@gmail.com>
2022-09-14 23:53:26 +0530

    Added unsplash API
    Added unsplash API

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-function validateform() {
-    let x = document.forms["myform"]["fname"].value;
-    if (x == "") {
-      alert("Name cannot be empty!");
-      return false;
-    }
-  }
-//   function displayDateandTime(){
-//    //document.getElementsById("demo").innerHTMl=Date();
-//    document.getElementById("demo").innerHTML = Date();
+// function validateform() {
+//     let x = document.forms["myform"]["fname"].value;
+//     if (x == "") {
+//       alert("Name cannot be empty!");
+//       return false;
+//     }
 //   }
-  function mouseOn(){
-    document.getElementById("box").innerHTML="Chal be chutiye!!";
-  }
-  function mouseOff(){
-    document.getElementById("box").innerHTML="Aye Lund Aye Vapas Aana Idhar!";
-  }
-  let element=document.getElementById("demo");
-    element.addEventListener("click", myFunction);
+// //   function displayDateandTime(){
+// //    //document.getElementsById("demo").innerHTMl=Date();
+// //    document.getElementById("demo").innerHTML = Date();
+// //   }
+//   function mouseOn(){
+//     document.getElementById("box").innerHTML="Chal be chutiye!!";
+//   }
+//   function mouseOff(){
+//     document.getElementById("box").innerHTML="Aye Lund Aye Vapas Aana Idhar!";
+//   }
+//   let element=document.getElementById("demo");
+//     element.addEventListener("click", myFunction);
 
-function myFunction() {
-  alert ("Hello World!");
-} 
+// function myFunction() {
+//   alert ("Hello World!");
+// } 
+let appId="9_jwuDWKrYfPAibVzfxcqZVd4oBXWwpnw13YCu1GbSk";
+let url=`https://api.unsplash.com/photos/?client_id=${appId}`;
+fetch(url)
+.then(function response(){
+    return response.json();
+})
+.then(function (jsondata){
+    console.log(jsondata);
+})
